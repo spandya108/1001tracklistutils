e1ed413f19dc7264ce816f8b5f58242d29ed71a0
Sahil Pandya <sahil.pandya108@gmail.com>
2014-07-05 17:04:28 -0700

    gitignore

--- listtracks/scrape.py
+++ listtracks/scrape.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
-import re
 
 
 class TrackListScrapeHandler(object):
 
     def execute_scrape(self, artists, year):
         scraper = TrackListScraper(artists, year)
-        scraper.execute_full_scrape()
+        full_list = scraper.execute_full_scrape()
+        return full_list
 
 
 class TrackListScraper(object):
@@ -20,9 +20,11 @@ class TrackListScraper(object):
         self.browser.visit('http://1001tracklists.com')
 
     def execute_full_scrape(self):
+        artist_tracklists = {}
         for artist in self.artists:
-            self.scrape_per_artist(artist)
+            artist_tracklists[artist] = self.scrape_per_artist(artist)
         self.browser.quit()
+        return artist_tracklists
 
     def scrape_per_artist(self, artist):
         """Execute the same scrape but instead using the python splinter library
@@ -34,7 +36,8 @@ class TrackListScraper(object):
 
         try:
             self.browser.click_link_by_partial_text('2014-06-')
-            self.get_track_list_for_set(artist)
+            track_strings = self.get_track_list_for_set(artist)
+            return track_strings
         except ElementDoesNotExist:
             pass
 
@@ -48,13 +51,14 @@ class TrackListScraper(object):
         for track in track_values:
             if track.a:
                 track_string = track.a.string
+                file.write(track_string)
                 # track details in format [artist, trackname]
                 track_details = self.parse_track_string(track_string)
                 track_strings.append(track_details)
         file.close()
+        return track_strings
 
     def parse_track_string(self, track_string):
-        print ("String before strip: " + track_string)
         track_info = track_string.strip().split('-')
         for i in range(len(track_info)):
             track_info[i] = track_info[i].strip()
